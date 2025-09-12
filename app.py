b4a963d341c5fecd2fd88360421b60a416179e7f
Anton Tumashov <Tumashovster@Gmail.com>
2025-09-12 11:27:06 +0500

    1.8 Виртуальное окружение, 'Black' 'F lake8'

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+"""
+Simple Flask web application for ShopFront.
+Returns contacts page on any GET request.
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-@app.route('/<path:subpath>', methods=['GET'])
-def contacts_page(subpath=None):
-    """Обрабатывает любой GET-запрос и возвращает страницу контактов"""
-    return render_template('contacts.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route("/", methods=["GET"])
+@app.route("/<path:subpath>", methods=["GET"])
+def contacts_page(_subpath=None):
+    """Обрабатывает любой GET-запрос и возвращает страницу контактов"""
+    return render_template("contacts.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
