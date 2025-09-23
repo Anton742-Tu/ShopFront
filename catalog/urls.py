bf2f5e36d2d7fb6030521229b616d2e93052cca8
Anton Tumashov <Tumashovster@Gmail.com>
2025-09-23 16:27:08 +0500

    4.8 - Удалили первичные файлы 'html', - Почистили файл со стилями, удалили дублирование, - Добавили обратную связь кнопки "Добавить продукт" (мигает синим), - Дополнненый 'README,md'

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name="home"),
-    path("catalog/", views.catalog_page, name="catalog"),
-    path("category/", views.category_page, name="category"),
-    path("contacts/", views.contacts_page, name="contacts"),
-    path("submit-form/", views.handle_form_submission, name="submit_form"),
-    path("product/<int:pk>/", views.product_detail, name="product_detail"),
-    path("product/add/", views.add_product, name="add_product"),
+    path('', views.home_page, name='home'),
+    path('catalog/', views.catalog_page, name='catalog'),
+    path('category/', views.category_page, name='category'),
+    path('contacts/', views.contacts_page, name='contacts'),
+    path('submit-form/', views.handle_form_submission, name='submit_form'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/add/', views.add_product, name='add_product'),
 ]
