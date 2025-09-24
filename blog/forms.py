4a89ad101609dad469e20d2ec26b116d8ed178fa
Anton Tumashov <Tumashovster@Gmail.com>
2025-09-24 15:23:09 +0500

    5.3 И так же точно прибрали импорты и исправили мелкие ошибки в коде с помощью 'black'

--- blog/forms.py
+++ blog/forms.py
@@ -1,39 +1,36 @@
 from django import forms
+
 from .models import BlogPost
 
 
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'slug', 'content', 'preview_image', 'is_published']
+        fields = ["title", "slug", "content", "preview_image", "is_published"]
         widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Введите заголовок'
-            }),
-            'slug': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Уникальный URL-адрес'
-            }),
-            'content': forms.Textarea(attrs={
-                'class': 'form-control',
-                'rows': 10,
-                'placeholder': 'Введите содержимое записи'
-            }),
-            'preview_image': forms.FileInput(attrs={
-                'class': 'form-control'
-            }),
-            'is_published': forms.CheckboxInput(attrs={
-                'class': 'form-check-input'
-            }),
+            "title": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Введите заголовок"}
+            ),
+            "slug": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Уникальный URL-адрес"}
+            ),
+            "content": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                    "rows": 10,
+                    "placeholder": "Введите содержимое записи",
+                }
+            ),
+            "preview_image": forms.FileInput(attrs={"class": "form-control"}),
+            "is_published": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
         labels = {
-            'title': 'Заголовок',
-            'slug': 'URL-адрес',
-            'content': 'Содержимое',
-            'preview_image': 'Превью',
-            'is_published': 'Опубликовать',
+            "title": "Заголовок",
+            "slug": "URL-адрес",
+            "content": "Содержимое",
+            "preview_image": "Превью",
+            "is_published": "Опубликовать",
         }
         help_texts = {
-            'slug': 'Уникальный идентификатор для URL (только латинские буквы, цифры, дефисы и подчеркивания)',
+            "slug": "Уникальный идентификатор для URL (только латинские буквы, цифры, дефисы и подчеркивания)",
         }
